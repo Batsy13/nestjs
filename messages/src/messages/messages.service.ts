57c801d8fd85109a243dfdce1d7ca676cb947c70
Pedro Costa <pedrocostavgs@gmail.com>
2025-12-31 14:21:00 -0300

    add Section 4.2: Nest Architecture: Services and Repositories ( DI )

--- messages/src/messages/messages.service.ts
+++ messages/src/messages/messages.service.ts
@@ -1,14 +1,10 @@
-import { MessageRepository } from "./messages.repository";
+import { Injectable } from "@nestjs/common";
+import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessageRepository;
 
-  constructor() {
-    // Service is creating its own dependencies
-    // DONT DO THIS ON REAL APPS
-    // USE DEPENDENCY INJECTION
-    this.messagesRepo = new MessageRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
