c128f8edc9ad5524daa5c8a310506a36d52b2e7b
Pedro Costa <pedrocostavgs@gmail.com>
2026-01-06 21:37:55 -0300

    add Section 10.1: Getting Started with Unit Testing

---
+++ car-value-api/src/users/auth.service.spec.ts
@@ -0,0 +1,67 @@
+import { Test } from "@nestjs/testing"
+import { AuthService } from "./auth.service"
+import { UsersService } from "./users.service";
+import { User } from "./user.entity";
+import { BadRequestException } from "@nestjs/common";
+
+describe("Auth Service", () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>
+
+  beforeEach(async () => {
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter(user => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = { id: Math.floor(Math.random() * 99999999), email, password } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      }
+
+    }
+
+    const module = await Test.createTestingModule({
+      providers: [AuthService, { provide: UsersService, useValue: fakeUsersService }]
+    }).compile();
+
+    service = module.get(AuthService);
+  })
+
+
+  it("can create an instance of auth service", async () => {
+    expect(service).toBeDefined();
+  })
+
+  it("creates a new user with a salted and hashed password", async () => {
+    const user = await service.signup("poggerstest@teste.com", "poggr");
+
+    expect(user.password).not.toEqual("poggr");
+    const [salt, hash] = user.password.split(".");
+    expect(salt).toBeDefined()
+    expect(hash).toBeDefined()
+  })
+
+  it("throws an error if user signs up with email that is in use", async () => {
+    await service.signup("poggerstest@teste.com", "poggr")
+    await expect(service.signup("poggerstest@teste.com", "poggr")).rejects.toThrow(BadRequestException)
+  })
+
+  it("throws an error if signin is called with an unused email", async () => {
+    await expect(service.signin("poggerstest@teste.com", "poggr")).rejects.toThrow(BadRequestException);
+  })
+
+  it("throws an error if an invalid password is provided", async () => {
+    await service.signup("poggerstest@teste.com", "poggr")
+
+    await expect(service.signin("poggerstest@teste.com", "poggr1")).rejects.toThrow(BadRequestException)
+  })
+
+  it("returns a user if correct password is provided", async () => {
+    await service.signup("poggerstest@teste.com", "poggr");
+    const user = await service.signin("poggerstest@teste.com", "poggr");
+    expect(user).toBeDefined();
+  })
+})
