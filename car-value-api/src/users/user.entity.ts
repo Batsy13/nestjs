58810547217798f356b9ccf00e5281b714cdd387
Pedro Costa <pedrocostavgs@gmail.com>
2026-01-11 11:16:28 -0300

    add Section 13: Relations with TypeORM

--- car-value-api/src/users/user.entity.ts
+++ car-value-api/src/users/user.entity.ts
@@ -1,5 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm"
-import { Exclude } from "class-transformer";
+import { Report } from "../reports/reports.entity";
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove, OneToMany } from "typeorm"
 
 @Entity()
 export class User {
@@ -11,6 +11,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
